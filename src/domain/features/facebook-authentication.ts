9063db7673201345595dd6a6a54b9374c311baa8
michaelquesado <esquesado@gmail.com>
2021-09-11 11:25:07 -0300

    add facebook authentication feature

---
+++ src/domain/features/facebook-authentication.ts
@@ -0,0 +1,13 @@
+import { AccessToken } from '@/domain/models'
+import { AuthenticationError } from '@/domain/errors'
+
+export interface FacebookAuthentication {
+  perform: (params: FacebookAuthentication.Params) => Promise<FacebookAuthentication.Result>
+}
+
+export namespace FacebookAuthentication {
+  export type Params = {
+    token: string
+  }
+  export type Result = AccessToken | AuthenticationError
+}
