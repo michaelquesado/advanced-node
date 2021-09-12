53780fb4d14ca1b9fc46e000313f526e2a97eb28
michaelquesado <esquesado@gmail.com>
2021-09-12 14:47:13 -0300

    ensure FacebookApi gets user infos

--- src/infra/apis/facebook.ts
+++ src/infra/apis/facebook.ts
@@ -19,11 +19,19 @@ export class FacebookApi {
         grant_type: 'client_credentials'
       }
     })
-    await this.httpGetClient.get({
+    const debugToken = await this.httpGetClient.get({
       url: `${this.baseUrl}/debug_token`,
       params: {
         access_token: appToken.access_token,
         input_token: params.token
+      }
+    })
+    await this.httpGetClient.get({
+      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+      url: `${this.baseUrl}/${debugToken.data.user_id}`,
+      params: {
+        fields: ['id', 'name', 'email'].join(','),
+        access_token: params.token
       }
     })
   }
