51437b89376472092f42a97d09515e3b5b45c041
michaelquesado <esquesado@gmail.com>
2021-09-12 15:42:32 -0300

    isolete SUT creation

--- tests/infra/http/axios-client.spec.ts
+++ tests/infra/http/axios-client.spec.ts
@@ -10,21 +10,26 @@ class AxiosClient {
   }
 }
 describe('AxiosHttpClient', () => {
+  let sut: AxiosClient
+  let fakeAxios: jest.Mocked<typeof axios>
+  let url: string
+  let params: object
+
+  beforeAll(() => {
+    url = 'any_url'
+    params = { any: 'any' }
+    fakeAxios = axios as jest.Mocked<typeof axios>
+  })
+
+  beforeEach(() => {
+    sut = new AxiosClient()
+  })
+
   describe('get', () => {
     it('should call get with correct params ', async () => {
-      const fakeAxios = axios as jest.Mocked<typeof axios>
-      const sut = new AxiosClient()
+      await sut.get({ url, params })
 
-      await sut.get({
-        url: 'any_url',
-        params: {
-          any: 'any'
-        }
-      })
-
-      expect(fakeAxios.get).toHaveBeenCalledWith('any_url', {
-        params: { any: 'any' }
-      })
+      expect(fakeAxios.get).toHaveBeenCalledWith(url, { params })
       expect(fakeAxios.get).toHaveBeenCalledTimes(1)
     })
   })
