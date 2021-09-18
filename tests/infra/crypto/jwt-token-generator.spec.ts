147241fb2602b0e81531d082e2501e28a94457fe
michaelquesado <esquesado@gmail.com>
2021-09-18 12:28:28 -0300

    isolete ISU creation

--- tests/infra/crypto/jwt-token-generator.spec.ts
+++ tests/infra/crypto/jwt-token-generator.spec.ts
@@ -13,9 +13,15 @@ class JwtTokenGenerator {
 }
 
 describe('JwtTokenGenerator', () => {
+  let sut: JwtTokenGenerator
+  let fakeJwt: jest.Mocked<typeof jwt>
+  beforeAll(() => {
+    fakeJwt = jwt as jest.Mocked<typeof jwt>
+  })
+  beforeEach(() => {
+    sut = new JwtTokenGenerator('secret')
+  })
   it('should call sign with correct params', async () => {
-    const fakeJwt = jwt as jest.Mocked<typeof jwt>
-    const sut = new JwtTokenGenerator('secret')
     await sut.generateToken({ key: 'any_key', expirationInMs: 1000 })
 
     expect(fakeJwt.sign).toHaveBeenCalledWith({ key: 'any_key' }, 'secret', { expiresIn: 1 })
