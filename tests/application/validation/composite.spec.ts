71c90e0a4696b9580b0e5b5f83f25e9147fa833f
michaelquesado <esquesado@gmail.com>
2021-09-21 22:36:29 -0300

    ensure CompositeValidator returns the first error

--- tests/application/validation/composite.spec.ts
+++ tests/application/validation/composite.spec.ts
@@ -1,13 +1,18 @@
 import { mock, MockProxy } from 'jest-mock-extended'
 
 interface Validator {
-  validate: () => undefined
+  validate: () => undefined | Error
 }
-class ValidationComposite {
+class ValidationComposite implements Validator {
   constructor (private readonly validators: Validator[]) {}
 
-  validate (): undefined {
-    return undefined
+  validate (): undefined | Error {
+    for (const validator of this.validators) {
+      const error = validator.validate()
+      if (error !== undefined) {
+        return error
+      }
+    }
   }
 }
 describe('ValidationComposite', () => {
@@ -30,5 +35,13 @@ describe('ValidationComposite', () => {
     const error = sut.validate()
 
     expect(error).toBeUndefined()
+  })
+
+  it('should return the first error', () => {
+    validator1.validate.mockReturnValue(new Error('error_1'))
+    validator2.validate.mockReturnValue(new Error('error_2'))
+    const error = sut.validate()
+
+    expect(error).toEqual(new Error('error_1'))
   })
 })
