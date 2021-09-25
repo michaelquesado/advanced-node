fa6e3b94dcbecd96548a005c4ebff720665a8ac5
michaelquesado <esquesado@gmail.com>
2021-09-25 16:44:18 -0300

    isolate SUT creation

--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -3,11 +3,16 @@ import { AxiosHttpClient } from '@/infra/http/axios-client'
 import { env } from '@/main/config/env'
 
 describe('FacebookApi Integration Tests', () => {
-  it('should return a Facebook User if token is valid', async () => {
-    const axiosClient = new AxiosHttpClient()
-    const sut = new FacebookApi(axiosClient, env.facebookApi.clientId, env.facebookApi.clientSecret)
+  let axiosClient: AxiosHttpClient
+  let sut: FacebookApi
+  const token = 'EAAFlhcjoNWwBALLLvCgVBSMSHLAyQ7qCWKK4hbrwnXCQBtJCXcgRReww4G7FYCippOYqZAbh6tMZCu7s72SJtDqLMbwE06PxGxRtNjKVMSihamLMCNrp6MoFpt980quQviCGyzcDmQv5EQGKBSZCURn26AbInbhZBU23JeX3fvrAJrRZCx441IaZARiXTwl7fhRZBZAZCh7tLAQcsN9lx4E41'
 
-    const fbUser = await sut.loadUser({ token: 'EAAFlhcjoNWwBALLLvCgVBSMSHLAyQ7qCWKK4hbrwnXCQBtJCXcgRReww4G7FYCippOYqZAbh6tMZCu7s72SJtDqLMbwE06PxGxRtNjKVMSihamLMCNrp6MoFpt980quQviCGyzcDmQv5EQGKBSZCURn26AbInbhZBU23JeX3fvrAJrRZCx441IaZARiXTwl7fhRZBZAZCh7tLAQcsN9lx4E41' })
+  beforeEach(() => {
+    axiosClient = new AxiosHttpClient()
+    sut = new FacebookApi(axiosClient, env.facebookApi.clientId, env.facebookApi.clientSecret)
+  })
+  it('should return a Facebook User if token is valid', async () => {
+    const fbUser = await sut.loadUser({ token })
 
     expect(fbUser).toEqual({
       facebookId: '100440632412559',
@@ -16,9 +21,6 @@ describe('FacebookApi Integration Tests', () => {
     })
   })
   it('should return undefined if token is invalid', async () => {
-    const axiosClient = new AxiosHttpClient()
-    const sut = new FacebookApi(axiosClient, env.facebookApi.clientId, env.facebookApi.clientSecret)
-
     const fbUser = await sut.loadUser({ token: 'invalid' })
 
     expect(fbUser).toBeUndefined()
