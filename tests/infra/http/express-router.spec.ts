3ff13695484606a9ac1515a3246d096bb43d7b4d
michaelquesado <esquesado@gmail.com>
2021-09-26 14:11:11 -0300

    transform ExpressRouter to a function

--- tests/infra/http/express-router.spec.ts
+++ tests/infra/http/express-router.spec.ts
@@ -1,31 +1,33 @@
 import { Controller } from '@/application/controllers'
-import { Request, Response } from 'express'
+import { NextFunction, Request, RequestHandler, Response } from 'express'
 
 import { getMockReq, getMockRes } from '@jest-mock/express'
 import { mock, MockProxy } from 'jest-mock-extended'
-import { ExpressRouter } from '@/infra/http'
+import { adaptExpressRoute } from '@/infra/http'
 
 describe('ExpressRouter', () => {
   let req: Request
   let res: Response
+  let next: NextFunction
   let controller: MockProxy<Controller>
-  let sut: ExpressRouter
+  let sut: RequestHandler
 
   beforeEach(() => {
     req = getMockReq({ body: { any: 'any' } })
     res = getMockRes().res
+    next = getMockRes().next
     controller = mock()
     controller.handle.mockResolvedValue({ statusCode: 200, data: { data: 'any_data' } })
-    sut = new ExpressRouter(controller)
+    sut = adaptExpressRoute(controller)
   })
   it('should call handle with correct request', async () => {
-    await sut.adapt(req, res)
+    await sut(req, res, next)
 
     expect(controller.handle).toHaveBeenCalledWith({ any: 'any' })
     expect(controller.handle).toHaveBeenCalledTimes(1)
   })
   it('should respond 200 if handle return 200', async () => {
-    await sut.adapt(req, res)
+    await sut(req, res, next)
 
     expect(res.status).toHaveBeenCalledWith(200)
     expect(res.status).toHaveBeenCalledTimes(1)
@@ -37,7 +39,7 @@ describe('ExpressRouter', () => {
       statusCode: 400,
       data: new Error('any_error')
     })
-    await sut.adapt(req, res)
+    await sut(req, res, next)
 
     expect(res.status).toHaveBeenCalledWith(400)
     expect(res.status).toHaveBeenCalledTimes(1)
@@ -49,7 +51,7 @@ describe('ExpressRouter', () => {
       statusCode: 500,
       data: new Error('any_error')
     })
-    await sut.adapt(req, res)
+    await sut(req, res, next)
 
     expect(res.status).toHaveBeenCalledWith(500)
     expect(res.status).toHaveBeenCalledTimes(1)
