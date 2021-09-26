be2c0fe633dacc2ab9d266015357b12d27ee586b
michaelquesado <esquesado@gmail.com>
2021-09-26 15:36:11 -0300

    add facebook login integration tests

--- src/infra/postgres/repos/user-account.ts
+++ src/infra/postgres/repos/user-account.ts
@@ -4,10 +4,9 @@ import { PgUser } from '@/infra/postgres/entities'
 import { getRepository } from 'typeorm'
 
 export class PgUserAccountRepository implements LoadUserAccountRepository, SaveFacebookAccountRepository {
-  private readonly userRepo = getRepository(PgUser)
-
   async load ({ email }: LoadUserAccountRepository.Params): Promise<LoadUserAccountRepository.Result> {
-    const pgUser = await this.userRepo.findOne({ email })
+    const userRepo = getRepository(PgUser)
+    const pgUser = await userRepo.findOne({ email })
     if (pgUser !== undefined) {
       return {
         id: pgUser.id.toString(),
@@ -17,12 +16,13 @@ export class PgUserAccountRepository implements LoadUserAccountRepository, SaveF
   }
 
   async saveWithFacebook ({ id, name, email, facebookId }: SaveFacebookAccountRepository.Params): Promise<SaveFacebookAccountRepository.Result> {
+    const userRepo = getRepository(PgUser)
     let requestId: string
     if (id === undefined) {
-      const userPg = await this.userRepo.save({ name, email, facebookId })
+      const userPg = await userRepo.save({ name, email, facebookId })
       requestId = userPg.id.toString()
     } else {
-      await this.userRepo.update({ id: +id }, { name, facebookId })
+      await userRepo.update({ id: +id }, { name, facebookId })
       requestId = id
     }
     return { id: requestId }
