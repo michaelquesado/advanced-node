62a3798ff6661436951d6df9fe045e9987940f15
michaelquesado <esquesado@gmail.com>
2021-09-27 21:33:19 -0300

    expressAdapter as functional

--- src/infra/http/express-router.ts
+++ src/infra/http/express-router.ts
@@ -1,13 +1,13 @@
 import { Controller } from '@/application/controllers'
 import { RequestHandler } from 'express'
 
-export const adaptExpressRoute = (controller: Controller): RequestHandler => {
-  return async (req, res) => {
-    const { data, statusCode } = await controller.handle({ ...req.body })
-    res
-      .status(statusCode)
-      .json(data.message !== undefined
-        ? { error: data.message }
-        : data)
-  }
+type Adapter = (controller: Controller) => RequestHandler
+
+export const adaptExpressRoute: Adapter = controller => async (req, res) => {
+  const { data, statusCode } = await controller.handle({ ...req.body })
+  res
+    .status(statusCode)
+    .json(data.message !== undefined
+      ? { error: data.message }
+      : data)
 }
