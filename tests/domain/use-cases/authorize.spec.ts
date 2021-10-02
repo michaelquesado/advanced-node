605fa93650cd3d6f51b69ed5da1ed548dd37afda
michaelquesado <esquesado@gmail.com>
2021-10-02 11:23:35 -0300

    ensure Authorize returns accessToken

--- tests/domain/use-cases/authorize.spec.ts
+++ tests/domain/use-cases/authorize.spec.ts
@@ -1,19 +1,21 @@
 import { mock, MockProxy } from 'jest-mock-extended'
 
 export interface TokenValidator {
-  validateToken: (params: TokenValidator.Params) => Promise<void>
+  validateToken: (params: TokenValidator.Params) => Promise<TokenValidator.Result>
 }
 
 export namespace TokenValidator {
   export type Params = { token: string }
+  export type Result = string
 }
 
 type Setup = (crypto: TokenValidator) => Authorize
 type Input = { token: string }
-type Authorize = (params: Input) => Promise<void>
+type Output = string
+type Authorize = (params: Input) => Promise<Output>
 
 const setupAuthorize: Setup = crypto => async params => {
-  await crypto.validateToken(params)
+  return await crypto.validateToken(params)
 }
 
 describe('Authorize', () => {
@@ -24,6 +26,7 @@ describe('Authorize', () => {
   beforeAll(() => {
     token = 'any_token'
     crypto = mock()
+    crypto.validateToken.mockResolvedValue('any_value')
   })
   beforeEach(() => {
     sut = setupAuthorize(crypto)
@@ -33,5 +36,9 @@ describe('Authorize', () => {
     await sut({ token })
     expect(crypto.validateToken).toHaveBeenCalledWith({ token })
     expect(crypto.validateToken).toHaveBeenCalledTimes(1)
+  })
+  it('should call return userId', async () => {
+    const userId = await sut({ token })
+    expect(userId).toBe('any_value')
   })
 })
