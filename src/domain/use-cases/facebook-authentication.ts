ddf50fcc6f55413ecf279190e4908ee1f09e8d40
michaelquesado <esquesado@gmail.com>
2021-10-02 11:18:17 -0300

    ensure Authorize calls TokenValidator with correct params

--- src/domain/use-cases/facebook-authentication.ts
+++ src/domain/use-cases/facebook-authentication.ts
@@ -5,7 +5,9 @@ import { FacebookAccount, AccessToken } from '@/domain/entities/index'
 import { TokenGenerator } from '@/domain/contracts/crypto/'
 
 type Setup = (facebookApi: LoadFacebookUserApi, userAccountRepo: LoadUserAccountRepository & SaveFacebookAccountRepository, crypto: TokenGenerator) => FacebookAuthentication
-export type FacebookAuthentication = (params: { token: string }) => Promise<{ accessToken: string }>
+type Input = { token: string }
+type Output = { accessToken: string }
+export type FacebookAuthentication = (params: Input) => Promise<Output>
 
 export const setupFacebookAuthenticationUseCase: Setup = (
   facebookApi,
