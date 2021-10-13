fa9dcde96243d07b764cc88c9e86243f9e92b7f9
michaelquesado <esquesado@gmail.com>
2021-10-12 22:29:31 -0300

    ensure UserProfile create correct entity

--- tests/domain/entities/user-profile.spec.ts
+++ tests/domain/entities/user-profile.spec.ts
@@ -1,8 +1,11 @@
 import { UserProfile } from '@/domain/entities/user-profile'
 
 describe('UserProfile', () => {
+  let sut: UserProfile
+  beforeEach(() => {
+    sut = new UserProfile('any_id')
+  })
   it('should create a UserProfile with empty initials when pictureUrl is provided', () => {
-    const sut = new UserProfile('any_id')
     sut.setPicture({
       pictureUrl: 'any_picture_url',
       name: 'any_name'
@@ -10,7 +13,51 @@ describe('UserProfile', () => {
     expect(sut).toEqual({
       id: 'any_id',
       pictureUrl: 'any_picture_url',
+      initials: undefined
+    })
+  })
+  it('should create a UserProfile with empty initials when pictureUrl is provided', () => {
+    sut.setPicture({
+      pictureUrl: 'any_picture_url',
       name: undefined
+    })
+    expect(sut).toEqual({
+      id: 'any_id',
+      pictureUrl: 'any_picture_url',
+      initials: undefined
+    })
+  })
+  it('should create a UserProfile with initials when pictureUrl is provided', () => {
+    sut.setPicture({
+      pictureUrl: undefined,
+      name: 'michael da silva quesado'
+    })
+    expect(sut).toEqual({
+      id: 'any_id',
+      pictureUrl: undefined,
+      initials: 'MQ'
+    })
+  })
+  it('should create a UserProfile with initials when pictureUrl is provided', () => {
+    sut.setPicture({
+      pictureUrl: undefined,
+      name: 'michael'
+    })
+    expect(sut).toEqual({
+      id: 'any_id',
+      pictureUrl: undefined,
+      initials: 'MI'
+    })
+  })
+  it('should create a UserProfile with initials when pictureUrl is provided', () => {
+    sut.setPicture({
+      pictureUrl: undefined,
+      name: 'm'
+    })
+    expect(sut).toEqual({
+      id: 'any_id',
+      pictureUrl: undefined,
+      initials: 'M'
     })
   })
 })
