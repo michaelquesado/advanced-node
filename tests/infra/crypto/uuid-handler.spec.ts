784a0f1b4078b089d3f7a492390ad342b229ed51
michaelquesado <esquesado@gmail.com>
2021-10-16 15:04:06 -0300

    isolate SUT creation

--- tests/infra/crypto/uuid-handler.spec.ts
+++ tests/infra/crypto/uuid-handler.spec.ts
@@ -2,7 +2,7 @@ import { UUIDGenerator } from '@/domain/contracts/gateways'
 import { mocked } from 'ts-jest/utils'
 import { v4 } from 'uuid'
 
-class UUIDHandler {
+class UUIDHandler implements UUIDGenerator {
   uuid ({ key }: UUIDGenerator.Params): UUIDGenerator.Output {
     return `${key}_${v4()}`
   }
@@ -10,17 +10,19 @@ class UUIDHandler {
 
 jest.mock('uuid')
 describe('UUIDHandler', () => {
+  let sut: UUIDHandler
+  beforeEach(() => {
+    sut = new UUIDHandler()
+  })
+  beforeAll(() => {
+    mocked(v4).mockReturnValue('any_uuid')
+  })
   test('should call uuid.v4', () => {
-    const sut = new UUIDHandler()
-
     sut.uuid({ key: 'any_key' })
 
     expect(v4).toHaveBeenCalledTimes(1)
   })
   test('should return a uuid', () => {
-    mocked(v4).mockReturnValueOnce('any_uuid')
-    const sut = new UUIDHandler()
-
     const result = sut.uuid({ key: 'any_key' })
 
     expect(result).toBe('any_key_any_uuid')
