1a1742abbfc7f8936d9e76a40b5cc094d497b202
michaelquesado <esquesado@gmail.com>
2021-10-17 13:53:57 -0300

    ensure AwsS3FileStorage calls deleteObject with correct params

--- src/infra/gateways/aws-s3-file-storage.ts
+++ src/infra/gateways/aws-s3-file-storage.ts
@@ -1,7 +1,7 @@
-import { UploadFile } from '@/domain/contracts/gateways'
+import { DeleteFile, UploadFile } from '@/domain/contracts/gateways'
 import { config, S3 } from 'aws-sdk'
 
-export class AwsS3FileStorage implements UploadFile {
+export class AwsS3FileStorage implements UploadFile, DeleteFile {
   constructor (accessKey: string, secret: string, private readonly bucket: string) {
     config.update({
       credentials: {
@@ -20,5 +20,13 @@ export class AwsS3FileStorage implements UploadFile {
       ACL: 'public-read'
     }).promise()
     return `https://${this.bucket}.s3.amazonaws.com/${encodeURIComponent(Key)}`
+  }
+
+  async delete ({ key: Key }: DeleteFile.Params): Promise<void> {
+    const s3 = new S3()
+    await s3.deleteObject({
+      Bucket: this.bucket,
+      Key
+    }).promise()
   }
 }
