2c4f2105329e6d107fe6976e39af57aa7662fbff
michaelquesado <esquesado@gmail.com>
2021-10-17 12:28:35 -0300

    ensure AWSS3FileStore returns imageURL

--- tests/infra/gateways/aws-s3-file-storage.spec.ts
+++ tests/infra/gateways/aws-s3-file-storage.spec.ts
@@ -3,7 +3,7 @@ import { config, S3 } from 'aws-sdk'
 import { mocked } from 'ts-jest/utils'
 
 jest.mock('aws-sdk')
-class AwsS3FileStorage {
+class AwsS3FileStorage implements UploadFile {
   constructor (accessKey: string, secret: string, private readonly bucket: string) {
     config.update({
       credentials: {
@@ -13,7 +13,7 @@ class AwsS3FileStorage {
     })
   }
 
-  async upload ({ key: Key, file: Body }: UploadFile.Params): Promise<void> {
+  async upload ({ key: Key, file: Body }: UploadFile.Params): Promise<UploadFile.Result> {
     const s3 = new S3()
     await s3.putObject({
       Bucket: this.bucket,
@@ -21,6 +21,7 @@ class AwsS3FileStorage {
       Body,
       ACL: 'public-read'
     }).promise()
+    return `https://${this.bucket}.s3.amazonaws.com/${encodeURIComponent(Key)}`
   }
 }
 describe('AwsS3FileStorage', () => {
@@ -39,8 +40,8 @@ describe('AwsS3FileStorage', () => {
     bucket = 'any_bucket'
     file = Buffer.from('any_buffer')
     putObjectPromiseSpy = jest.fn()
-    putObjectSpy = jest.fn().mockImplementationOnce(() => ({ promise: putObjectPromiseSpy }))
-    mocked(S3).mockImplementationOnce(jest.fn().mockImplementationOnce(() => ({
+    putObjectSpy = jest.fn().mockImplementation(() => ({ promise: putObjectPromiseSpy }))
+    mocked(S3).mockImplementation(jest.fn().mockImplementation(() => ({
       putObject: putObjectSpy
     })))
   })
@@ -68,5 +69,15 @@ describe('AwsS3FileStorage', () => {
     })
     expect(putObjectSpy).toHaveBeenCalledTimes(1)
     expect(putObjectPromiseSpy).toHaveBeenCalledTimes(1)
+  })
+  it('should return imageUrl', async () => {
+    const urlImage = await sut.upload({ key, file })
+
+    expect(urlImage).toBe(`https://${bucket}.s3.amazonaws.com/${key}`)
+  })
+  it('should return encoded imageUrl', async () => {
+    const urlImage = await sut.upload({ key: 'any key', file })
+
+    expect(urlImage).toBe(`https://${bucket}.s3.amazonaws.com/any%20key`)
   })
 })
