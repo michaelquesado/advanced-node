834002f017cabe853d763890e2c0be8220e7b7a2
michaelquesado <esquesado@gmail.com>
2021-10-21 08:06:47 -0300

    ensure SavePictureController returns 400 if mimeType is invalid

--- tests/application/controllers/save-picture.spec.ts
+++ tests/application/controllers/save-picture.spec.ts
@@ -1,16 +1,30 @@
 import { RequiredFieldError } from '@/application/errors'
 import { HttpResponse, badRequest } from '@/application/helpers'
 
-type HttpRequest = { file: { buffer: Buffer } }
+type HttpRequest = { file: { buffer: Buffer, mimeType: string } }
 type Model = Error
 
 class SavePictureController {
   async handle ({ file }: HttpRequest): Promise<HttpResponse<Model>> {
-    return badRequest(new RequiredFieldError('file'))
+    if (file === undefined || file === null || file.buffer.length < 1) return badRequest(new RequiredFieldError('file'))
+    return badRequest(new InvalidMimeTypeError(['jpeg', 'png']))
+  }
+}
+
+class InvalidMimeTypeError extends Error {
+  constructor (allowedTypes: string[]) {
+    super(`Unsupported type. Allowed: ${allowedTypes.join(', ')}`)
+    this.name = 'InvalidMimeTypeError'
   }
 }
 describe('SavePictureController', () => {
   let sut: SavePictureController
+  let buffer: Buffer
+  let mimeType: string
+  beforeAll(() => {
+    buffer = Buffer.from('any_buffer')
+    mimeType = 'image/jpeg'
+  })
   beforeEach(() => {
     sut = new SavePictureController()
   })
@@ -31,11 +45,19 @@ describe('SavePictureController', () => {
     })
   })
   it('should return 400 if file is empty', async () => {
-    const httpResponse = await sut.handle({ file: { buffer: Buffer.from('') } })
+    const httpResponse = await sut.handle({ file: { buffer: Buffer.from(''), mimeType } })
 
     expect(httpResponse).toEqual({
       statusCode: 400,
       data: new RequiredFieldError('file')
+    })
+  })
+  it('should return 400 if mimeType is invalid', async () => {
+    const httpResponse = await sut.handle({ file: { buffer, mimeType: 'invalid' } })
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new InvalidMimeTypeError(['jpeg', 'png'])
     })
   })
 })
