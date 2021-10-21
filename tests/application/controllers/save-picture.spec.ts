ef6f25372e86412893898fb180a27c21fa9f76ae
michaelquesado <esquesado@gmail.com>
2021-10-21 07:47:26 -0300

    isolate sut creation

--- tests/application/controllers/save-picture.spec.ts
+++ tests/application/controllers/save-picture.spec.ts
@@ -1,7 +1,7 @@
 import { RequiredFieldError } from '@/application/errors'
 import { HttpResponse, badRequest } from '@/application/helpers'
 
-type HttpRequest = { file: any }
+type HttpRequest = { file: { buffer: Buffer } }
 type Model = Error
 
 class SavePictureController {
@@ -10,10 +10,20 @@ class SavePictureController {
   }
 }
 describe('SavePictureController', () => {
+  let sut: SavePictureController
+  beforeEach(() => {
+    sut = new SavePictureController()
+  })
   it('should return 400 if file is undefined', async () => {
-    const sut = new SavePictureController()
+    const httpResponse = await sut.handle({ file: undefined as any })
 
-    const httpResponse = await sut.handle({ file: undefined })
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new RequiredFieldError('file')
+    })
+  })
+  it('should return 400 if file is null', async () => {
+    const httpResponse = await sut.handle({ file: null as any })
 
     expect(httpResponse).toEqual({
       statusCode: 400,
