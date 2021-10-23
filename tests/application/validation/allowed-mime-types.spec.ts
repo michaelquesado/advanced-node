dd352cfc64a94b0c0d830e83d0d75dadbf33b341
michaelquesado <esquesado@gmail.com>
2021-10-23 11:11:38 -0300

    ensure AllowedMimeTypes returns undefined if mimeType is valid

--- tests/application/validation/allowed-mime-types.spec.ts
+++ tests/application/validation/allowed-mime-types.spec.ts
@@ -8,10 +8,11 @@ class AllowedMimeTypes {
     private readonly mimeType: string
   ) {}
 
-  validate (): Error {
-    return new InvalidMimeTypeError(this.allowed)
+  validate (): Error | undefined {
+    if (this.allowed.includes('png') && this.mimeType !== 'image/png') return new InvalidMimeTypeError(this.allowed)
   }
 }
+
 describe('AllowedMimeTypes', () => {
   it('should return InvalidMimeTypeError if mimeType is invalid', () => {
     const sut = new AllowedMimeTypes(['png'], 'image/jpeg')
@@ -19,5 +20,12 @@ describe('AllowedMimeTypes', () => {
     const error = sut.validate()
 
     expect(error).toEqual(new InvalidMimeTypeError(['png']))
+  })
+  it('should return undefined if mimeType is valid', () => {
+    const sut = new AllowedMimeTypes(['png'], 'image/png')
+
+    const error = sut.validate()
+
+    expect(error).toBeUndefined()
   })
 })
