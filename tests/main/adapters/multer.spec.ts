4beaaaf63c502735913ab8b39f32539371e7708d
michaelquesado <esquesado@gmail.com>
2021-11-07 16:45:47 -0300

    ensure MulterAdapter returns 500 if multer fails

--- tests/main/adapters/multer.spec.ts
+++ tests/main/adapters/multer.spec.ts
@@ -3,10 +3,13 @@ import { RequestHandler, Request, Response, NextFunction } from 'express'
 import { mocked } from 'ts-jest/utils'
 
 import multer from 'multer'
+import { ServerError } from '@/application/errors'
 
 const multerAdapter: RequestHandler = (req, res, next) => {
   const upload = multer().single('picture')
-  upload(req, res, () => {})
+  upload(req, res, err => {
+    res.status(500).json({ error: new ServerError(err).message })
+  })
 }
 
 jest.mock('multer')
@@ -23,11 +26,11 @@ describe('MulterAdapter', () => {
   let next: NextFunction
 
   beforeAll(() => {
-    uploadSpy = jest.fn()
-    singleSpy = jest.fn().mockImplementationOnce(() => uploadSpy)
-    multerSpy = jest.fn().mockImplementationOnce(() => ({ single: singleSpy }))
+    uploadSpy = jest.fn().mockImplementation(() => {})
+    singleSpy = jest.fn().mockImplementation(() => uploadSpy)
+    multerSpy = jest.fn().mockImplementation(() => ({ single: singleSpy }))
     fakeMulter = multer as jest.Mocked<typeof multer>
-    mocked(fakeMulter).mockImplementationOnce(multerSpy)
+    mocked(fakeMulter).mockImplementation(multerSpy)
 
     sut = multerAdapter
     req = getMockReq()
@@ -44,5 +47,16 @@ describe('MulterAdapter', () => {
     expect(singleSpy).toHaveBeenCalledTimes(1)
     expect(uploadSpy).toHaveBeenCalledWith(req, res, expect.any(Function))
     expect(uploadSpy).toHaveBeenCalledTimes(1)
+  })
+  it('should return 500 if upload fails', () => {
+    const error = new Error('multer_error')
+    uploadSpy.mockImplementationOnce((req, res, next) => next(error))
+
+    sut(req, res, next)
+
+    expect(res.status).toHaveBeenCalledWith(500)
+    expect(res.status).toHaveBeenCalledTimes(1)
+    expect(res.json).toHaveBeenCalledWith({ error: new ServerError(error).message })
+    expect(res.json).toHaveBeenCalledTimes(1)
   })
 })
