cdeb02df9de5359721217e3d64b4180f27096fb7
michaelquesado <esquesado@gmail.com>
2021-11-13 14:15:37 -0300

    correcao da conexao

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,9 +1,13 @@
 import './config/module-alias'
 
 import 'reflect-metadata'
-import { app } from '@/main/config/app'
-import { createConnections } from 'typeorm'
+import { createConnection, getConnectionOptions } from 'typeorm'
 
-createConnections()
-  .then(() => app.listen(8000, () => console.log('Server running at http://localhost:8000')))
-  .catch(console.error)
+getConnectionOptions().then(async options => {
+  const root = process.env.TS_NODE_DEV === undefined ? 'dist' : 'src'
+  const entities = [`${root}/infra/repos/postgres/entities/index.{js,ts}`]
+  await createConnection({ ...options, entities })
+  console.log({ ...options, entities })
+  const { app } = await import('@/main/config/app')
+  app.listen(8000, () => console.log('Server running at http://localhost:8000'))
+}).catch(console.error)
--- tests/infra/repos/postgres/mocks/connection.ts
+++ tests/infra/repos/postgres/mocks/connection.ts
@@ -4,7 +4,7 @@ export const makeFakeDB = async (entities?: any[]): Promise<IMemoryDb> => {
   const db = newDb()
   const connection = await db.adapters.createTypeormConnection({
     type: 'postgres',
-    entities: entities ?? ['src/infra/postgres/entities/index.ts']
+    entities: entities ?? ['src/infra/repos/rpostgres/entities/index.ts']
   })
   await connection.synchronize()
   return db
