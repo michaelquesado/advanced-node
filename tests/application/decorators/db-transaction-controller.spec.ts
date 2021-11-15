d57a4d65ce49a6e56df600c4c1bc8fdd77185812
michaelquesado <esquesado@gmail.com>
2021-11-15 10:33:11 -0300

    ensuere DbTransactionController calls openTransaction

---
+++ tests/application/decorators/db-transaction-controller.spec.ts
@@ -0,0 +1,24 @@
+import { mock } from 'jest-mock-extended'
+
+class DbTransactionController {
+  constructor (private readonly db: DbTransaction) {}
+
+  async perform (httpRequest: any): Promise<void> {
+    await this.db.openTransaction()
+  }
+}
+
+interface DbTransaction {
+  openTransaction: () => Promise<void>
+}
+describe('DbTransactionController', () => {
+  it('should call open transaction', async () => {
+    const db = mock<DbTransaction>()
+    const sut = new DbTransactionController(db)
+
+    await sut.perform({ any: 'any' })
+
+    expect(db.openTransaction).toHaveBeenCalledWith()
+    expect(db.openTransaction).toHaveBeenCalledTimes(1)
+  })
+})
